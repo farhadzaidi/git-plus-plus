23b9d6ff2c7e5a58c54f70a898f2dafc2b828a84
Farhad Zaidi <zaidi.farhad03@gmail.com>
2025-11-08 01:42:27 -0500

    some ideas

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,6 +3,21 @@ export const GPP = 'gpp';
 export const COMMANDS = {
 	REBUILD: 'rebuild',
 	GOTO: 'goto',
+	START: 'start', // replace checkout -b
+	PUBLISH: 'publish', // replace git add . && git commit -m "message" && git push
+	DELETE: 'delete', // remove a branch - enable interactivity
+	RENAME: 'rename', // rename a branch - enable interactivity
+	BACK: 'back', // go back to previous commit safely
+	FORWARD: 'forward', // move to next commit
+	// be able to interactively select commits?
+	// check out files from other branches easier
+	// auto set upstream?
+	// simpler remote vs local differentiation
+	// Interaticely stage and unstage files
+	// wipe - show a dry run and ask to confirm
+	// unstaged - interactively choose or specify
+	// staged - interactively choose or specify
+	// all - git reset --hard
 };
 
 export const COMMAND_ALIASES = {};
