9e9cdc9d454a4283f481869ee62ead6c6a18f335
Farhad Zaidi <zaidi.farhad03@gmail.com>
2025-11-08 20:09:35 -0500

    update

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,26 +1,32 @@
 export const GPP = 'gpp';
 
 export const COMMANDS = {
+	HEALTH: 'health', // ensures gpp 
+	CONFIG: 'config', // show currently supported commands
+	VERISON: 'version', // show version
+	UPDATE: 'update', // reinstall npm package globally
 	REBUILD: 'rebuild',
 	GOTO: 'goto',
-	CREATE: 'create', // replace checkout -b
-	PUBLISH: 'publish', // replace git add . && git commit -m "message" && git push
-	DELETE: 'delete', // remove a branch - enable interactivity
-	RENAME: 'rename', // rename a branch - enable interactivity
-	// auto set upstream? (git push -u origin HEAD)
-	// uncommit? (git reset --soft HEAD~1)
-	// Interaticely stage and unstage files
-	// - choose from untracked files to stage certain ones
-	// - choose from tracked files to unstage certain ones
-	// BACK: 'back', // go back to previous commit safely ?
-	// FORWARD: 'forward', // move to next commit ?
+	CREATE: 'create',
+	PUBLISH: 'publish',
+	DELETE: 'delete', // remove one or more branches - use multiselect and confirm
+	RENAME: 'rename', // rename a branch - use select, input, and confirm
+	TRACK: 'track', // auto set upstream (git push -u origin HEAD)
+	UNCOMMIT: 'uncommit', // safe uncommit (git reset --soft HEAD~1)
+	PICK: 'pick', // choose from unstage (including untracked) files to stage - multiselect and confirm
+	DROP: 'drop', // choose from staged files to unstage - multiselect and confirm
+
+	// wipe - show a dry run and ask to confirm
+	// --unstaged - interactively choose or specify (clean -fd)
+	// --staged - interactively choose or specify (restore)
+	// --all - git reset --hard
+
+	// go back to previous commit safely ?
+	// move to next commit ?
 	// be able to interactively select commits?
 	// check out files from other branches easier? (interactively?)
 	// simpler remote vs local differentiation (?)
-	// wipe - show a dry run and ask to confirm
-	// - unstaged - interactively choose or specify
-	// - staged - interactively choose or specify
-	// - all - git reset --hard
+
 };
 
 export const GITCONFIG_ALIASES = {
